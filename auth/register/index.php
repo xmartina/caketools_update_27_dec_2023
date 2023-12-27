d9410229c2229d525805bcb1d5fd171b5208fec1
xmartina <martinsprince126@gmail.com>
2023-12-27 04:31:32 +0100

    init

--- auth/register/index.php
+++ auth/register/index.php
@@ -8,10 +8,7 @@ include_once (rootDir.'includes/generalConfig.php');
 include_once (rootDir.'includes/auth.php');
 include_once (rootDir.'partials/auth/header.php');
 
-if (isset($_SESSION['user_id'])) {
-    header("Location:" .siteUrl."users");
-    exit();
-}
+
 ?>
 <section class="tf-page-title style-2">
     <div class="tf-container">
