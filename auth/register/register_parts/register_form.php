977363057ba953bcbf1fad1ef2dc50697517e015
xmartina <martinsprince126@gmail.com>
2023-12-27 04:17:59 +0100

    init

---
+++ auth/register/register_parts/register_form.php
@@ -0,0 +1,33 @@
+<form action="" method="post">
+    <div class="row">
+        <div class="col-lg-6">
+            <fieldset><input id="name" name="first_name" tabindex="1" aria-required="true" required="" type="text" placeholder="First name"></fieldset>
+        </div>
+        <div class="col-lg-6">
+            <fieldset><input id="name" name="last_name" tabindex="2" aria-required="true" required="" type="text" placeholder="Last name"></fieldset>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <fieldset><input id="name" name="user_name" tabindex="1" aria-required="true" required="" type="text" placeholder="User name"></fieldset>
+        </div>
+        <div class="col-lg-6">
+            <fieldset><input id="name" name="email" tabindex="1" aria-required="true" required="" type="email" placeholder="Email"></fieldset>
+        </div>
+    </div>
+    <fieldset> <input id="showpassword" name="password" tabindex="2" aria-required="true"  type="password" placeholder="Password" required="">
+        <span class="btn-show-pass"><i class="far fa-eye-slash"></i></span></fieldset>
+    <fieldset class="mb24"> <input id="showpassword2" name="password" tabindex="2" aria-required="true"  type="password" placeholder="Confirm password" required="">
+        <span class="btn-show-pass2"><i class="far fa-eye-slash"></i></span></fieldset>
+    <div class="forgot-pass-wrap">
+        <label>I agree to the terms and services
+            <input type="checkbox">
+            <span class="btn-checkbox"></span>
+        </label>
+    </div>
+    <div class="title-login">Or login with social</div>
+    <div class="button-gg"><a href="#" ><i class="fab fa-facebook"></i>Facebook</a></div>
+    <div class="button-gg mb33"><a href="#" ><i class="fab fa-google"></i>Google</a>
+    </div>
+    <button class="submit" type="submit">Login</button>
+</form>
