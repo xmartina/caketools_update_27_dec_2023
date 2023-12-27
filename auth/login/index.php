6a4f556d9fa00c868243a1f8fd32ba53c59f6c19
xmartina <martinsprince126@gmail.com>
2023-12-27 03:40:13 +0100

    init

--- auth/login/index.php
+++ auth/login/index.php
@@ -2,7 +2,7 @@
     session_start();
     const pageName = 'Login';
     const rootDir = '/home/multistream6/domains/caketoolnftmarketplace.com/public_html/';
-    include_once ('includes/generalConfig.php');
+    include_once (rootDir.'includes/generalConfig.php');
     include_once ('includes/core.php');
     include_once (rootDir.'partials/auth/header.php');
 ?>
