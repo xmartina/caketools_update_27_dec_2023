64faa53b027d0f089046b4c281713592f68c8763
xmartina <martinsprince126@gmail.com>
2023-12-27 14:33:40 +0100

    init

--- partials/users/footer/main.php
+++ partials/users/footer/main.php
@@ -108,6 +108,19 @@
 <a id="scroll-top"></a>
 
 <!-- Javascript -->
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        // Get the target element
+        var targetElement = document.getElementById("targetElement");
+
+        // Set interval to remove the 'is-fixed' class every 5 seconds
+        if (targetElement) {
+            setInterval(function() {
+                targetElement.querySelector('.dashboard-user').classList.remove("is-fixed");
+            }, 5); // 5000 milliseconds = 5 seconds
+        }
+    });
+</script>
 <script src="https://unpkg.com/imagesloaded@5/imagesloaded.pkgd.min.js"></script>
 <script src="https://unpkg.com/imagesloaded@5/imagesloaded.pkgd.js"></script>
 <script src="/assets/js/jquery.min.js"></script>
