db3537bfa407043a896a40e9e9b0ebef1b53762b
xmartina <martinsprince126@gmail.com>
2023-12-28 17:35:34 +0100

    init

--- users/sidebar/main.php
+++ users/sidebar/main.php
@@ -6,11 +6,11 @@
             </div>
             <div class="name">Francisco Maia</div>
             <div class="pax mb-2"><i class="fas fa-dollar-sign"></i>My Balance</div>
-            <div class="pax"><i class="fab fa-bitcoin"></i>0.053</div>
-            <div class="pax"><i class="fab fa-ethereum"></i>0.4399</div>
-            <div class="pax"><i class="">₮</i>0.4399</div>
+            <div class="pax mb-2"><i class="fab fa-bitcoin"></i>0.053</div>
+            <div class="pax mb-3"><i class="fab fa-ethereum"></i>0.4399</div>
+            <div class="pax mb-2"><i class="">₮</i>0.4399</div>
             <div class="description">
-                8,888 NFTs of beautiful, Asian women painstakingly-crafted where even the most intricate
+                Explore your account: Instantly view balances and recent activity for a clear and concise financial snapshot.
             </div>
             <ul class="social-item">
                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
