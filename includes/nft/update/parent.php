9a9ae4ba01882d3dbf1c20c9d02bcddda6473ec4
xmartina <martinsprince126@gmail.com>
2023-12-29 11:53:34 +0100

    init

--- includes/nft/update/parent.php
+++ includes/nft/update/parent.php
@@ -1,13 +1,13 @@
 <?php
 // Update NFT parent
+if (isset($_POST['update_nft'])) {
+    $new_nft_name = $_POST['name'];
+    $new_nft_description = $_POST['description'];
+    $new_nft_price = $_POST['price'];
+    $new_nft_category = $_POST['category'];
+    $new_nft_likes = $_POST['likes'];
 
-$new_nft_name = $_POST['name'];
-$new_nft_description = $_POST['description'];
-$new_nft_price = $_POST['price'];
-$new_nft_category = $_POST['category'];
-$new_nft_likes = $_POST['likes'];
-
-$updateSql = "UPDATE nft_parent SET
+    $updateSql = "UPDATE nft_parent SET
     name = '$new_nft_name',
     description = '$new_nft_description',
     price = '$new_nft_price',
@@ -15,10 +15,11 @@ $updateSql = "UPDATE nft_parent SET
     likes = '$new_nft_likes'
     WHERE ref_id = '$nft_ref_id'";
 
-$result = $conn->query($updateSql);
+    $result = $conn->query($updateSql);
 
-if ($result === true) {
-    echo "NFT Parent records updated successfully<br>";
-} else {
-    echo "Error updating NFT Parent records: " . $conn->error . "<br>";
+    if ($result === true) {
+        echo "NFT Parent records updated successfully<br>";
+    } else {
+        echo "Error updating NFT Parent records: " . $conn->error . "<br>";
+    }
 }
