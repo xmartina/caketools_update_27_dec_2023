e4b0ac420dea73d2b3c91c573b0b258af7887ac3
xmartina <martinsprince126@gmail.com>
2023-12-29 10:42:50 +0100

    init

--- includes/nft/create/parent.php
+++ includes/nft/create/parent.php
@@ -1,30 +1,32 @@
 <?php
-//Create New NFT Record
+// Create New NFT Record
 if (isset($create_new_nft)) {
     $length = 16;
     $string_gen = $length;
-    include_once ('includes/helper/code_generator.php');
+    include_once('includes/helper/code_generator.php');
 
     $new_nft_name = $_POST['name'];
     $new_nft_description = $_POST['description'];
     $new_nft_price = $_POST['price'];
     $new_nft_category = $_POST['category'];
     $new_nft_likes = $_POST['likes'];
+
+    // Check if owner_id and ref_code are set before using them
+    $owner_id = isset($_GET['owner_id']) ? $_GET['owner_id'] : $_SESSION['user_id'];
+    $ref_code = generateRandomString($length);
+
     if ($directory == '/admin/edit_nft/ref_id/') {
-        $insertSql = "INSERT INTO nft_parent (created_user_id, ref_id, current_owner_id, name, description, price, category, likes, date_created, time_created) 
-              VALUES (?, ?, ?, ?, ?, ?,?,?, CURRENT_DATE(), CURRENT_TIME())";
+        // Your additional code for '/admin/edit_nft/ref_id/' here...
+        // This block is executed if $directory is '/admin/edit_nft/ref_id/'
+    }
 
-        $insertStmt = $conn->prepare($insertSql);
-        $insertStmt->bind_param("isssisss", $owner_id, $ref_code, $owner_id,  $nft_name, $nft_description, $nft_price, $nft_category, $nft_likes);
+    $insertSql = "INSERT INTO nft_parent (created_user_id, ref_id, current_owner_id, name, description, price, category, likes, date_created, time_created) 
+                  VALUES (?, ?, ?, ?, ?, ?, ?, ?, CURRENT_DATE(), CURRENT_TIME())";
 
-// Assign values to variables like $nft_name, $nft_description, etc. before executing the statement
+    $insertStmt = $conn->prepare($insertSql);
 
-        $nft_name = $new_nft_name;
-        $owner_id = $_GET['owner_id'];
-        $nft_description = $new_nft_description;
-        $nft_price = $new_nft_price;
-        $nft_category = $new_nft_category;
-        $nft_likes = $new_nft_likes;
+    if ($insertStmt) {
+        $insertStmt->bind_param("issisiss", $owner_id, $ref_code, $owner_id, $new_nft_name, $new_nft_description, $new_nft_price, $new_nft_category, $new_nft_likes);
 
         $insertStmt->execute();
 
@@ -36,31 +38,9 @@ if (isset($create_new_nft)) {
 
         $insertStmt->close();
     } else {
-        $owner_id = $_SESSION['user_id'];
-        $insertSql = "INSERT INTO nft_parent (created_user_id, ref_id, current_owner_id, name, description, price, category, likes, date_created, time_created) 
-              VALUES (?, ?, ?, ?, ?, ?,?,?, CURRENT_DATE(), CURRENT_TIME())";
-
-        $insertStmt = $conn->prepare($insertSql);
-        $insertStmt->bind_param("isssisss", $owner_id, $ref_code, $owner_id,  $nft_name, $nft_description, $nft_price, $nft_category, $nft_likes);
-
-// Assign values to variables like $nft_name, $nft_description, etc. before executing the statement
-
-        $nft_name = $new_nft_name;
-        $owner_id = $_SESSION['user_id'];
-        $nft_description = $new_nft_description;
-        $nft_price = $new_nft_price;
-        $nft_category = $new_nft_category;
-        $nft_likes = $new_nft_likes;
-
-        $insertStmt->execute();
-
-        if ($insertStmt->affected_rows > 0) {
-            echo "NFT Parent record inserted successfully. ID: " . $insertStmt->insert_id . "<br>";
-        } else {
-            echo "Error inserting NFT Parent record: " . $insertStmt->error . "<br>";
-        }
-
-        $insertStmt->close();
+        echo "Error preparing the statement.";
     }
 
+    // Additional statement for '/admin/edit_nft/ref_id/' after the NFT Parent insertion...
 }
+?>
