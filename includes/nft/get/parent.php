9354da19db168e32b5a201735aa7ca5c6bd9cfa5
xmartina <martinsprince126@gmail.com>
2023-12-29 16:49:57 +0100

    init

--- includes/nft/get/parent.php
+++ includes/nft/get/parent.php
@@ -1,11 +1,11 @@
 <?php
-if ($directory == '/admin/edit_nft/ref_id/') {
-    $sql = "SELECT * FROM nft_parent WHERE ref_id = $nft_ref_id";
-    return $sql;
-} else {
+//if ($directory == '/admin/edit_nft/ref_id/') {
+//    $sql = "SELECT * FROM nft_parent WHERE ref_id = $nft_ref_id";
+//    return $sql;
+//} else {
 //Get NFT Parent data
     $sql = "SELECT * FROM nft_parent WHERE current_owner_id = $user_id";
-    return $sql;
+//    return $sql;
 }
 //$sql = "SELECT * FROM nft_parent WHERE ref_id = $nft_ref_id OR current_owner_id = $user_id";
 $result = $conn->query($sql);
