fb695bd983bd54ce27236c7326c23f04a8329cda
xmartina <martinsprince126@gmail.com>
2023-12-29 11:36:18 +0100

    init

--- includes/nft/update/parent.php
+++ includes/nft/update/parent.php
@@ -1,45 +1,28 @@
 <?php
 // Update NFT parent
+$updateSql = "UPDATE nft_parent SET
+name = ?,
+description = ?,
+price = ?,
+category = ?,
+likes = ?
+WHERE ref_id = ? OR current_owner_id = ?";
+
+$updateStmt = $conn->prepare($updateSql);
+
 if ($directory == edit_nft_admin) {
-    $updateSql = "UPDATE nft_parent SET
-        name = ?,
-        description = ?,
-        price = ?,
-        category = ?,  
-        likes = ?
-        WHERE ref_id = ? OR current_owner_id = ?";
-
-    $updateStmt = $conn->prepare($updateSql);
-    $updateStmt->bind_param("ssssiii", $new_nft_name, $new_nft_description, $new_nft_price, $new_nft_category, $new_nft_likes, $nft_current_owner_id, $nft_ref_id);
-    return $updateStmt;
-
+$updateStmt->bind_param("ssssii", $new_nft_name, $new_nft_description, $new_nft_price, $new_nft_category, $new_nft_likes, $nft_ref_id);
 } else {
-    // Get NFT Parent data
-    $updateSql = "UPDATE nft_parent SET
-        name = ?,
-        description = ?,
-        price = ?,
-        category = ?,  
-        likes = ?
-        WHERE current_owner_id = ? ";
-
-    $updateStmt = $conn->prepare($updateSql);
-    $updateStmt->bind_param("ssssii", $new_nft_name, $new_nft_description, $new_nft_price, $new_nft_category, $new_nft_likes, $nft_current_owner_id);
-
-    return $updateStmt;
-
+// Get NFT Parent data
+$updateStmt->bind_param("ssssii", $new_nft_name, $new_nft_description, $new_nft_price, $new_nft_category, $new_nft_likes, $nft_current_owner_id);
 }
-//
-//$updateStmt = $conn->prepare($updateSql);
-//$updateStmt->bind_param("ssssiii", $new_nft_name, $new_nft_description, $new_nft_price, $new_nft_category, $new_nft_likes, $nft_current_owner_id, $nft_ref_id);
 
 $updateStmt->execute();
 
 if ($updateStmt->affected_rows > 0) {
-    echo "NFT Parent records updated successfully for owner ID: $nft_current_owner_id <br>";
+echo "NFT Parent records updated successfully for owner ID: $nft_current_owner_id <br>";
 } else {
-    echo "Error updating NFT Parent records for owner ID: $nft_current_owner_id <br>";
+echo "Error updating NFT Parent records for owner ID: $nft_current_owner_id <br>";
 }
 
 $updateStmt->close();
-
