f5033f1d9aa2dee5c79264ea3f9a000b6495e524
xmartina <martinsprince126@gmail.com>
2023-12-29 16:53:44 +0100

    init

--- includes/nft/get/category.php
+++ includes/nft/get/category.php
@@ -1,13 +1,13 @@
 <?php
 //Get Category data
-if ($directory == '/admin/edit_nft/ref_id/') {
+//if ($directory == '/admin/edit_nft/ref_id/') {
+//$sql = "SELECT * FROM category WHERE c_id = $nft_category_id";
+//return $sql;
+//} else {
+////Get NFT Parent data
 $sql = "SELECT * FROM category WHERE c_id = $nft_category_id";
-return $sql;
-} else {
-//Get NFT Parent data
-$sql = "SELECT * FROM category WHERE c_id = $nft_category_id";
-return $sql;
-}
+//return $sql;
+//}
 //$sql = "SELECT * FROM nft_parent WHERE ref_id = $nft_ref_id OR current_owner_id = $user_id";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
