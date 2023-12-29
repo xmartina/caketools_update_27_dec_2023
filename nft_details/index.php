2a56b0713f91dddf8cdae22e66abd0b967c80369
xmartina <martinsprince126@gmail.com>
2023-12-29 14:48:31 +0100

    init

--- nft_details/index.php
+++ nft_details/index.php
@@ -14,17 +14,17 @@ include_once (rootDir.'includes/core.php');
 include_once (rootDir.'partials/users/header/main.php');
 ?>
 
-
+<div class="py-5"></div>
 <section class="tf-page-title ">
             <div class="tf-container">
                 <div class="row">
                     <div class="col-md-12">
 
-                        <ul class="breadcrumbs">
-                            <li><a href="index.html">Home</a></li>
-                            <li><a href="explore.html">Explore</a></li>
-                            <li>Item Details</li>
-                        </ul>
+<!--                        <ul class="breadcrumbs">-->
+<!--                            <li><a href="index.html">Home</a></li>-->
+<!--                            <li><a href="explore.html">Explore</a></li>-->
+<!--                            <li>Item Details</li>-->
+<!--                        </ul>-->
                         <h4 class="page-title-heading">Item Details</h4>
                     </div>
                 </div>
