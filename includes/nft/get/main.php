df3e7dbe5391e855d520bacd49f910c677eccdb7
xmartina <martinsprince126@gmail.com>
2023-12-29 16:56:20 +0100

    init

--- includes/nft/get/main.php
+++ includes/nft/get/main.php
@@ -2,3 +2,4 @@
 include_once (rootDir.'includes/nft/get/parent.php');
 include_once (rootDir.'includes/nft/get/properties.php');
 include_once (rootDir.'includes/nft/get/bids.php');
+include_once (rootDir.'includes/nft/get/category.php');
