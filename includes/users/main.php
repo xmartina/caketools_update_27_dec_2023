048c22b22793f51f66cbdd2c7de4b02c7abd5b03
xmartina <martinsprince126@gmail.com>
2023-12-30 01:39:19 +0100

    init

--- includes/users/main.php
+++ includes/users/main.php
@@ -1,8 +1,4 @@
 <?php
-if (!isset($user_id)) {
-//    include_once(rootDir . 'includes/users/get/get_users_details.php');
-    include_once(rootDir . 'includes/users/get/nft_current_owner.php');
-}
 if (isset($user_id)) {
     include_once(rootDir . 'includes/users/get/get_users_details_for_auth_user.php');
 }
