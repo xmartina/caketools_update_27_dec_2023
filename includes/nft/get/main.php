8bf8d0be57e6352caf4cb9679f1edf23f72663de
xmartina <martinsprince126@gmail.com>
2023-12-30 01:01:46 +0100

    init

--- includes/nft/get/main.php
+++ includes/nft/get/main.php
@@ -4,3 +4,4 @@ include_once (rootDir.'includes/nft/get/properties.php');
 include_once (rootDir.'includes/nft/get/bids.php');
 include_once (rootDir.'includes/nft/get/category.php');
 include_once (rootDir.'includes/nft/get/nft_current_owner.php');
+include_once (rootDir.'includes/nft/get/nft_creator.php');
