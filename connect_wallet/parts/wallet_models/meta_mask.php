0caa4104e4017d101e7cc3e7b012dd58a35a6d90
xmartina <martinsprince126@gmail.com>
2024-01-02 00:48:34 +0100

    init

--- connect_wallet/parts/wallet_models/meta_mask.php
+++ connect_wallet/parts/wallet_models/meta_mask.php
@@ -45,49 +45,49 @@ if ($result->num_rows > 0) {
         // Display the modal structure
         ?>
         <?php if ($wallet_status == 0) { ?>
-        <!-- The Modal -->
-        <div class="modal fade" id="<?=$wallet_key?>">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
+            <!-- The Modal -->
+            <div class="modal fade" id="<?=$wallet_key?>">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
 
-                    <!-- Modal Header -->
-                    <div class="modal-header">
-                        <h4 class="modal-title"><?=$wallet_name?></h4>
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <!-- Modal Header -->
+                        <div class="modal-header">
+                            <h4 class="modal-title"><?=$wallet_name?></h4>
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+
+                        <!-- Modal Body -->
+                        <div class="modal-body">
+                            <form id="myForm">
+                                <div class="form-group">
+                                    <label for="input1">Input 1:</label>
+                                    <input type="text" class="form-control" id="input1" placeholder="Enter Input 1">
+                                </div>
+                                <div class="form-group">
+                                    <label for="input2">Input 2:</label>
+                                    <input type="text" class="form-control" id="input2" placeholder="Enter Input 2">
+                                </div>
+                                <button type="button" class="btn btn-primary" onclick="submitForm()">Submit</button>
+
+                                <!-- Loader -->
+                                <div class="spinner-border text-primary mt-3" role="status" id="loader" style="display: block;">
+                                    <span class="sr-only">Loading...</span>
+                                </div>
+
+                                <!-- Error Message -->
+                                <div id="errorMessage" class="text-danger mt-3" style="display: none;"></div>
+                            </form>
+                        </div>
+
+                        <!-- Modal Footer -->
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+
                     </div>
-
-                    <!-- Modal Body -->
-                    <div class="modal-body">
-                        <form id="myForm">
-                            <div class="form-group">
-                                <label for="input1">Input 1:</label>
-                                <input type="text" class="form-control" id="input1" placeholder="Enter Input 1">
-                            </div>
-                            <div class="form-group">
-                                <label for="input2">Input 2:</label>
-                                <input type="text" class="form-control" id="input2" placeholder="Enter Input 2">
-                            </div>
-                            <button type="button" class="btn btn-primary" onclick="submitForm()">Submit</button>
-
-                            <!-- Loader -->
-                            <div class="spinner-border text-primary mt-3" role="status" id="loader" style="display: block;">
-                                <span class="sr-only">Loading...</span>
-                            </div>
-
-                            <!-- Error Message -->
-                            <div id="errorMessage" class="text-danger mt-3" style="display: none;"></div>
-                        </form>
-                    </div>
-
-                    <!-- Modal Footer -->
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-
                 </div>
             </div>
-        </div>
-        <?php }elseif ($wallet_status == 1) { ?>
+        <?php } elseif ($wallet_status == 1) { ?>
             <!-- The Modal -->
             <div class="modal fade" id="<?=$wallet_key?>">
                 <div class="modal-dialog modal-dialog-centered">
@@ -113,7 +113,7 @@ if ($result->num_rows > 0) {
                     </div>
                 </div>
             </div>
-        <?php }  ?>
+        <?php } ?>
         <?php
     }
 } else {
