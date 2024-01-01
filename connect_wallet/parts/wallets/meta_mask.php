4d6dac269c162e377f145ff235a5397baea473cd
xmartina <martinsprince126@gmail.com>
2024-01-02 00:05:57 +0100

    init

--- connect_wallet/parts/wallets/meta_mask.php
+++ connect_wallet/parts/wallets/meta_mask.php
@@ -4,7 +4,7 @@
             <img src="/assets/images/svg/<?= $wallet_img ?>" alt="Image">
             <span class="label"><?=$wallet_status?></span>
         </div>
-        <h6 class="title"><a href="#"> <?=$wallet_key ." ".$wallet_status?></a></h6>
+        <h6 class="title"><a href="#"> <?=$wallet_key?></a></h6>
         <p class="content">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters</p>
     </div>
 </div>
