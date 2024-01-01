ddfc62b8b4d2a8e660e8d0f2435d952a075baffc
xmartina <martinsprince126@gmail.com>
2024-01-01 09:41:11 +0100

    init

--- includes/nft/get/main.php
+++ includes/nft/get/main.php
@@ -5,3 +5,4 @@ include_once (rootDir.'includes/nft/get/bids.php');
 include_once (rootDir.'includes/nft/get/category.php');
 include_once (rootDir.'includes/nft/get/nft_current_owner.php');
 include_once (rootDir.'includes/nft/get/nft_creator.php');
+include_once (rootDir.'includes/nft/get/nft_currency.php');
