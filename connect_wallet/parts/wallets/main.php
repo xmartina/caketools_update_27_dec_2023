6fbf607a59862814787a795101b54ce72d15c75c
xmartina <martinsprince126@gmail.com>
2024-01-02 00:26:27 +0100

    init

--- connect_wallet/parts/wallets/main.php
+++ connect_wallet/parts/wallets/main.php
@@ -1,7 +1,2 @@
 <?php
 include_once (rootDir.'connect_wallet/parts/wallets/meta_mask.php');
-include_once (rootDir.'connect_wallet/parts/wallets/wallet_connect.php');
-include_once (rootDir.'connect_wallet/parts/wallets/coin_base.php');
-include_once (rootDir.'connect_wallet/parts/wallets/binance.php');
-include_once (rootDir.'connect_wallet/parts/wallets/bitski.php');
-
