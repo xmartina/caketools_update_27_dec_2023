522018e04c50cdc2d278111f000da33a9e1ff68b
xmartina <martinsprince126@gmail.com>
2024-01-02 01:59:08 +0100

    init

--- assets/js/front_footer_js.js
+++ assets/js/front_footer_js.js
@@ -5,17 +5,17 @@ $("#logoutBtn").click(function() {
 
 // Wallet Connect form validation
 // Define the submitForm function before it is referenced
-function submitForm() {
-    // Display loader
-    document.getElementById("loader").style.display = "block";
-
-    // Simulate a 3-second delay (adjust as needed)
-    setTimeout(function() {
-        // Hide loader
-        document.getElementById("loader").style.display = "none";
-
-        // Display error message
-        document.getElementById("errorMessage").innerHTML = "Unable to connect wallet. Please try again.";
-        document.getElementById("errorMessage").style.display = "block";
-    }, 3000); // 3000 milliseconds = 3 seconds
-}
+// function submitForm() {
+//     // Display loader
+//     document.getElementById("loader").style.display = "block";
+//
+//     // Simulate a 3-second delay (adjust as needed)
+//     setTimeout(function() {
+//         // Hide loader
+//         document.getElementById("loader").style.display = "none";
+//
+//         // Display error message
+//         document.getElementById("errorMessage").innerHTML = "Unable to connect wallet. Please try again.";
+//         document.getElementById("errorMessage").style.display = "block";
+//     }, 3000); // 3000 milliseconds = 3 seconds
+// }
