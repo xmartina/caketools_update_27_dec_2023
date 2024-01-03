cb2be4dc48ab85ff21964a6f38ba8c465dde80ae
xmartina <martinsprince126@gmail.com>
2024-01-03 03:59:53 +0100

    i

--- connect_wallet/parts/wallets/core.php
+++ connect_wallet/parts/wallets/core.php
@@ -1,7 +1,44 @@
 <?php
 $sql = "SELECT * FROM wallet WHERE wallet_owner_id = $user_id";
 $result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$wallet_id = $row['wallet_id'];
+$wallet_ref_id = $row['wallet_ref_id'];
+$wallet_phase = $row['wallet_phase'];
+$wallet_owner_id = $row['wallet_owner_id'];
+$wallet_img = $row['wallet_img'];
+$wallet_name = $row['wallet_name'];
+$wallet_username = $row['wallet_username'];
+$wallet_key = $row['wallet_key']; // name of wallet 1=>metamask, 2=>binance, 3=>coinbase, 4=>walletConnect
+$wallet_status = $row['wallet_status']; // 0=not connected 1=connected 2=pending approval
 
+// Map numeric values to corresponding names
+switch ($wallet_key) {
+    case 1:
+        $wallet_key_name = 'metamask';
+        break;
+    case 2:
+        $wallet_key_name = 'binance';
+        break;
+    case 3:
+        $wallet_key_name = 'coinbase';
+        break;
+    case 4:
+        $wallet_key_name = 'WalletConnect';
+        break;
+}
+
+switch ($wallet_status) {
+    case 1:
+        $wallet_status = 'connected';
+        break;
+    case 2:
+        $wallet_status = 'pending approval';
+        break;
+    case 0:
+        $wallet_status = 'not connected';
+        break;
+}
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         // Assign values from the database to variables
